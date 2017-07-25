1bdb40b94e77b64ed270457c11e3e0d15ba6488c
José Senart <zecams@gmail.com>
2017-07-25 16:31:31 +0200

    repulsion working

--- sim2.py
+++ sim2.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 import matplotlib.pyplot as plt
 from graphics import *
@@ -10,7 +10,7 @@ from random import *
 import numpy as np
 
 
-# In[5]:
+# In[2]:
 
 def initialize(randspeeds = True, N = 100, width = 1000, height = 1000):
 
@@ -65,7 +65,6 @@ def next_step(agents, speeds, dt, N):
         agents[i].move(dxvec[i], dyvec[i])
         
 
-
 def plot(agents, width, height):
     win = GraphWin("Swarm", width, height) # size of box
     for agent in agents:
@@ -75,14 +74,38 @@ def plot(agents, width, height):
     #win.close()
 
 
-# In[6]:
+# In[3]:
 
-def couzin():
+def couzin(agents, speeds, N, rr=1, ro=2, ra=3):
     # watch only particles inrepuls
-    
-    return
+    distances = [get_distances(agent, agents) for agent in agents]
+    for i in range(N): ### FIX - Eliminate, in some way the i-i interaction
+        r_agents = []
+        o_agents = []
+        a_agents = []
+        repulsion_flag = False    
+        for j in range(N):
+            if i == j:
+                continue
+            if distances[i][j] < rr:
+                temp_vec = [agents[j].getX()-agents[i].getX(), agents[j].getY()-agents[i].getY()]
+                norm_vec = map(lambda x: x/distances[i][j], temp_vec)
+                r_agents.append(norm_vec)
+                repulsion_flag = True
+            elif not repulsion_flag and distances[i][j] < ro:
+                
+                
+                pass
+        
+        if repulsion_flag:
+            rep_direction = np.sum(r_agents, axis=0)
+            print(rep_direction)
+            rep_direction = map(lambda x: x/np.sqrt(rep_direction[0]**2+rep_direction[1]**2), rep_direction)
 
-
+            speeds[i] = map(add, speeds[i], rep_direction) # bias in the direction
+            speeds[i] = map(lambda x: x*np.sqrt(speeds[i][0]**2+speeds[i][1]**2), rep_direction) # re-normalization 
+        
+        
 
 
 def vicsek():
@@ -92,7 +115,7 @@ def gueron():
     return
 
 
-# In[ ]:
+# In[4]:
 
 def simulate(N_steps = 10, a = 0.1, dt = 0.01, N = 100, width = 1000, height = 1000, randspeeds = True):
     """
@@ -110,14 +133,15 @@ def simulate(N_steps = 10, a = 0.1, dt = 0.01, N = 100, width = 1000, height = 1
     agents, speeds, window = initialize(randspeeds, N)
     for i in range(N_steps):
         next_step(agents, speeds, dt, N)
-        couple_speeds(agents, speeds, a, N)
+        couzin(agents, speeds, N, 10,20,30)
+        #couple_speeds(agents, speeds, a, N)
         treat_boundary(width, height, agents, speeds, N)
     window.close()
 
 simulate(N_steps = 100, a =0.1, dt = 10)
 
 
-# In[39]:
+# In[ ]:
 
 from graphics import *
 
