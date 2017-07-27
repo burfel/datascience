852cbe5ded7c89881493da17eab3c9e6a2a76538
frederikeis <frederikeis@hotmail.com>
2017-07-27 17:15:39 +0100

    bias and accuracy

--- sim3.py
+++ sim3.py
@@ -1,17 +1,19 @@
 
 # coding: utf-8
 
-# In[112]:
+# In[ ]:
 
 import matplotlib.pyplot as plt
 from graphics import *
 from operator import *
 from random import *
+#from visual import *
 import numpy as np
-import time
+import timeit
+from math import *
 
 
-# In[113]:
+# In[ ]:
 
 def initialize(speed = 1, N = 100, width = 1000, height = 1000):
     """
@@ -24,10 +26,10 @@ def initialize(speed = 1, N = 100, width = 1000, height = 1000):
         speeds[i][0] = speed * np.cos(theta)
         speeds[i][1] = speed * np.sin(theta)
 
-    return agents, speeds, plot(agents, width, height)
+    return agents, speeds, draw_agents(agents, width, height)
 
 
-def plot(agents, width, height):
+def draw_agents(agents, width, height):
     win = GraphWin("Swarm", width, height) # size of box
     for agent in agents:
         agent.draw(win)
@@ -111,16 +113,13 @@ def periodic_boundary(x_bound, y_bound, agents, speeds, N):  #Changed from rigid
             dy = y_bound
 
         agents[i].move(dx, dy)
-        
-def get_v_avg(speeds, N):
-    return
 
 def next_step(agents, speeds, dt, N):
     dxvec = [dt * speeds[i][0] for i in range(N)]
     dyvec = [dt * speeds[i][1] for i in range(N)]
     for i in range(N):
         agents[i].move(dxvec[i], dyvec[i])
-
+        
 
 def normalized(vector):
     if vector[0] == vector[1] == 0:
@@ -128,9 +127,33 @@ def normalized(vector):
     return vector / np.linalg.norm(vector)
 
 
-# In[114]:
+#def get_v_avg(speeds, N):
+#    return
+#   
+#def get_potencials(distances, N):
+#    potentials = np.zeros(N, N)
+#    for i in range(N):
+#        for j in range(i+1,N):
+#            potentials[i][j] = potentials[j][i] = morse_pot(distances[i][j])
+#    return potentials  
+#    
+#def morse_pot(r, ca = 0.5, cr = 1, la = 3, lr = 0.5):
+#    return cr*np.exp(-r/lr) - ca * np.exp(-r)
+#
+#def weird_paper(agents, N, dt):
+#    distances = [get_distances(agent, agents) for agent in agents]
+#    potencials = get_potencials(distances, N)
+#    for i in range(N):
+#            newSpeed = speed[i] + dt * potentials
+#            speeds[i] = speeds[i] + weightedSpeed
+#            speeds[i] = s * normalized(speeds[i]) ### PROBLEMA?-------------------------------------------------
+#
+#    return
 
-def couzin(agents, speeds, N, s, rr=1, ro=2, ra=3):
+
+# In[66]:
+
+def couzin(agents, speeds, N, s, noise, rr=1, ro=2, ra=3):
     # watch only particles inrepuls
     for i in range(N): ### FIX - Eliminate, in some way the i-i interaction
         curr_agent = agents[i]
@@ -160,7 +183,7 @@ def couzin(agents, speeds, N, s, rr=1, ro=2, ra=3):
                     temp_vec = normalized(temp_vec)
                     a_dir = a_dir + temp_vec
             
-                    
+        #Out of for (j), we treat now the resulting direction vector
         if repulsion_flag:
             tot_dir =  normalized(-r_dir)
         else:
@@ -168,32 +191,56 @@ def couzin(agents, speeds, N, s, rr=1, ro=2, ra=3):
             a_dir = normalized(a_dir)
             tot_dir = o_dir + a_dir
             tot_dir = normalized(tot_dir)
-
+            
+        tot_dir = normalized(normalized(tot_dir) + noise * np.array([(-1)+2*random(),(-1)+2*random()]))
+        
+        #avoid pts stoping when not interacting
         if np.linalg.norm(tot_dir) != 0:
             speeds[i] = s*tot_dir 
 
+def gbias(speeds, N, s, prop, bias, weight):
+    #bias = np.array([0.0,1.0])
+    #Ns has to be integer 
+    Ns = int(N * prop)
+    for i in range(Ns):
+        tot_dir = normalized(normalized(speeds[i]) + weight * bias)
+        
+        if np.linalg.norm(tot_dir) != 0:
+            speeds[i] = s*tot_dir
 
+def accuracy(speeds, N, bias):
+    dev = 0.0
+    for i in range(N):
+        dev = dev + np.arccos(np.dot(bias,normalized(speeds[i])))
+    dev_avg = dev/(N*2*(np.pi))
+    print (dev_avg)
+    
+def elongation(agents, N):
+    cn = np.array([0.0, 0.0])
+    for i in range(N):
+        cn = agents
+    
 def vicsek(agents, speeds, N, s, noise, r): # s=speed, noise= letter csi temperature factor, r=radius of interaction
     # consider only particles within 'r' from pt_i, align pt_i with v_avg
     for i in range(N):
         distances = get_distances(agents[i], agents)
-        o_dir = np.array([0.0, 0.0])
+        tot_dir = np.array([0.0, 0.0])
         
         for j in range(N):
             if distances[j] < r:
-                o_dir = o_dir + speeds[j]
+                tot_dir = tot_dir + speeds[j]
 
-        o_dir = s * normalized(normalized(o_dir) + noise * np.array([(-1)+2*random(),(-1)+2*random()]))
-        if np.linalg.norm(o_dir) != 0:
-            speeds[i] = o_dir
+        tot_dir = s * normalized(normalized(tot_dir) + noise * np.array([(-1)+2*random(),(-1)+2*random()]))
+        if np.linalg.norm(tot_dir) != 0:
+            speeds[i] = tot_dir
 
 def gueron():
     return
 
 
-# In[119]:
+# In[70]:
 
-def simulate(N_steps, a, dt, N, width, height, s):
+def simulate(N_steps, a, dt, N, width, height, s, noise, prop, weight, bias):
     """
     Simulates motion of swarm. Recieves following parameters:
 
@@ -205,15 +252,18 @@ def simulate(N_steps, a, dt, N, width, height, s):
     s - module of speed throughout agents
 
     """
-
     agents, speeds, window = initialize(s, N, width, height)
     for i in range(N_steps):
         next_step(agents, speeds, dt, N)
-        
+        import timeit
+
+        start = timeit.default_timer()
         ## MODEL
-        vicsek(agents, speeds, N, s, noise = 0.9, r = 500)
-        #couzin(agents, speeds, N, s,10,1900,2000)
-        #couple_speeds(agents, speeds, a, s, N
+        #couple_speeds(agents, speeds, a, s, N)
+        #vicsek(agents, speeds, N, s, 0.1, r = 500)
+        couzin(agents, speeds, N, s, noise,100,1900,2000)
+        gbias(speeds, N, s, prop, bias, weight)
+        accuracy(speeds, N, bias)
         
         ## BOUNDARY CONDITIONS
         #treat_boundary(width, height, agents, speeds, N)
@@ -221,9 +271,18 @@ def simulate(N_steps, a, dt, N, width, height, s):
         #print(v_avg)
         #v_avg = get_v_avg(speeds)
         #time.sleep(0.02)
+        stop = timeit.default_timer()
+        #print stop - start
     window.close()
+    
 
-simulate(2000, 0.1, 1, 100, 500, 500, 2)
+#simulate(N_steps, a, dt, N, width, height, s, noise, prop, weight, bias)
+simulate(2000, 0.1, 1, 50, 500, 500, 2, 0.02, 0.4,0.5, np.array([0.0,-1.0]))
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
